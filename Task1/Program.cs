1456a50bdd826ed256359878f7b128cbcecadb0c
Vladimir Shytsko <shytsko.v@gmail.com>
2022-04-14 00:50:19 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int inputNumber = new Random().Next(100, 1000);
+int outputNumber = inputNumber % 10;
+
+Console.WriteLine("Входное число: " + inputNumber);
+Console.WriteLine("Последняя цифра входного числа: " + outputNumber);
